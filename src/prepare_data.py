d40f36a4dda3e593a6c2997c92d3a26ab7fbde3e
mekadmini <mohamedislamm@gmail.com>
2026-01-21 19:51:57 +0100

    refactoring

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,7 +1,11 @@
+import os
+
 from multivae.data.datasets import MnistSvhn
 
 # --- 1. SETUP DATA ---
 # multivae handles the download and pairing of MNIST-SVHN automatically
+script_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_dir, "..", "data")
 print("Setting up MNIST-SVHN dataset...")
-train_data = MnistSvhn(data_path="../data", split="train", download=True)
-test_data = MnistSvhn(data_path="../data", split="test", download=True)
+train_data = MnistSvhn(data_path=data_path, split="train", download=True)
+test_data = MnistSvhn(data_path=data_path, split="test", download=True)
